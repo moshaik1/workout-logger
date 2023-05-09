34d25802adcfb23c2aad76dbec4928808b0a00c5
moshaik1 <mrsrnz@umsystem.edu>
2023-05-09 17:40:45 -0500

    redirecting and routing

--- workout-logger/src/App.js
+++ workout-logger/src/App.js
@@ -3,20 +3,37 @@ import "./App.css";
 import Signup from "./components/account/Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./components/contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/account/Dashboard";
+import Login from "./components/account/Login";
+import PrivateRoute from "./components/account/PrivateRoute";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <h1 className="text-center">WORKOUT LOGGER</h1>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
